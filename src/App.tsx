8afb80f4b44fbf27a6bcd75fb7c20eb62f791398
wangliuyuan <wangliuyuanperson@163.com>
2022-06-10 18:55:25 +0800

    modify:1、登录开始

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React, { FC } from 'react';
 import Animation from './views/Login/Animation';
+import './app.less';
+import { LoginConfig } from './views/Login';
 const App: FC<any> = (props) => {
 	return (
 		<div className='app'>
 			<Animation />
+			<LoginConfig />
 		</div>
 	);
 };
---
+++ src/views/Login/index.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from 'react';
+
+const LoginConfig: FC<any> = (props) => {
+	return <div className='login-config'></div>;
+};
+
+export { LoginConfig };
